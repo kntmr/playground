7300f928b419799466a2579869921ea651d0212d
kntmr <beatkent@gmail.com>
2017-01-12 23:39:10 +0900

    add List#replaceAll example

--- java-examples/src/test/java/com/github/kntmr/CollectionAPITests.java
+++ java-examples/src/test/java/com/github/kntmr/CollectionAPITests.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.UnaryOperator;
 
 public class CollectionAPITests {
 
@@ -33,6 +34,14 @@ public class CollectionAPITests {
         System.out.println(">> " + list);
         list.sort((s1, s2) -> s2.compareTo(s1)); // 逆順
         System.out.println("<< " + list);
+    }
+
+    @Test
+    public void test04() {
+        List<Integer> list = Arrays.asList(1, 2, 3);
+        UnaryOperator<Integer> operator = i -> i + 1;
+        list.replaceAll(operator);
+        list.forEach(System.out::println);
     }
 
 }
