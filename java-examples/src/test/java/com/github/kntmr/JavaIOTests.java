e0a4adb7eaf0f2ca626770a84f4aea8cfa1b90de
kntmr <beatkent@gmail.com>
2017-03-14 00:31:06 +0900

    Files.list and Files.walk examples

--- java-examples/src/test/java/com/github/kntmr/JavaIOTests.java
+++ java-examples/src/test/java/com/github/kntmr/JavaIOTests.java
@@ -5,6 +5,10 @@ import org.junit.Test;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 public class JavaIOTests {
 
@@ -17,6 +21,26 @@ public class JavaIOTests {
             System.out.print(br.readLine());
             br.reset();
             System.out.print(br.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void test02() {
+        try (Stream<Path> stream = Files.list(Paths.get("dir"))) { // 指定ディレクトリ直下のみ
+            stream.forEach(System.out::println);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void test03() {
+        try (Stream<Path> stream = Files.walk(Paths.get("dir"))) { // 指定ディレクトリ配下を再帰的に探索
+            stream.forEach(System.out::println);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
