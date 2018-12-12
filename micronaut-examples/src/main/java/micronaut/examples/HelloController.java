6c24d4a8ab353c523e09cc0a9e2947d96cf6f903
kntmr <beatkent@gmail.com>
2018-12-12 09:29:44 +0900

    add controller

---
+++ micronaut-examples/src/main/java/micronaut/examples/HelloController.java
@@ -0,0 +1,15 @@
+package micronaut.examples;
+
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.HttpStatus;
+
+@Controller("/hello")
+public class HelloController {
+
+    @Get("/")
+    public HttpStatus index() {
+        return HttpStatus.OK;
+    }
+
+}
