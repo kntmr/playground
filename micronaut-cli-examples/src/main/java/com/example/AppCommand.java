5d28553abf57520f1e943250746549baae1158c6
kntmr <1657438+kntmr@users.noreply.github.com>
2019-11-30 10:25:35 +0900

    example

--- micronaut-cli-examples/src/main/java/com/example/AppCommand.java
+++ micronaut-cli-examples/src/main/java/com/example/AppCommand.java
@@ -1,28 +1,45 @@
 package com.example;
 
-import io.micronaut.configuration.picocli.PicocliRunner;
-import io.micronaut.context.ApplicationContext;
+import java.io.File;
+import java.util.concurrent.Callable;
 
-import picocli.CommandLine;
+import io.micronaut.configuration.picocli.PicocliRunner;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Mixin;
 import picocli.CommandLine.Option;
-import picocli.CommandLine.Parameters;
 
 @Command(name = "app", description = "...",
         mixinStandardHelpOptions = true)
-public class AppCommand implements Runnable {
+public class AppCommand implements Callable<Integer> {
 
-    @Option(names = {"-v", "--verbose"}, description = "...")
-    boolean verbose;
+    @Mixin
+    private Options options;
+
+    static class Options {
+        @Option(names = {"-v", "--verbose"}, description = "...")
+        boolean verbose;
+        @Option(names = {"-a"}, split = ",")
+        String[] array = {};
+        @Option(names = {"-f"})
+        File file;
+    }
 
     public static void main(String[] args) throws Exception {
-        PicocliRunner.run(AppCommand.class, args);
+        //PicocliRunner.run(AppCommand.class, args);
+        int exitStatus = PicocliRunner.execute(AppCommand.class, args);
+        System.exit(exitStatus);
     }
 
     public void run() {
         // business logic here
-        if (verbose) {
+        if (options.verbose) {
             System.out.println("Hi!");
         }
     }
+
+    @Override
+    public Integer call() throws Exception {
+        return 0;
+    }
+
 }
