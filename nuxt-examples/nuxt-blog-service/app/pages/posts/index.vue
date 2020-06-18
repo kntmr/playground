0f8abca772783dd03ab56856f973d540d2306fcc
kntmr <1657438+kntmr@users.noreply.github.com>
2020-06-19 01:13:53 +0900

    example 1 (layout)

---
+++ nuxt-examples/nuxt-blog-service/app/pages/posts/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <section class="container posts-page">
+    <el-card>
+      <div slot="header" class="clearfix">
+        <span>新着投稿</span>
+      </div>
+      <el-table :data="showPosts" class="table" style="width:100%">
+        <el-table-column prop="title" label="タイトル"></el-table-column>
+        <el-table-column prop="user.id" label="投稿者" width="180"></el-table-column>
+        <el-table-column prop="created_at" label="投稿日時" width="240"></el-table-column>
+      </el-table>
+    </el-card>
+  </section>
+</template>
+
+<script>
+export default {
+  computed: {
+    showPosts () {
+      return [
+        {
+          id: '001',
+          title: 'Title 1',
+          body: 'Content 1 ...',
+          created_at: '2020/06/18 12:30:00',
+          user: {
+            id: 'kntmr'
+          }
+        },
+        {
+          id: '002',
+          title: 'Title 2',
+          body: 'Content 2 ...',
+          created_at: '2020/06/18 18:45:00',
+          user: {
+            id: 'kntmr'
+          }
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+.posts-page .el-table__row {
+  cursor: pointer;
+}
+</style>
