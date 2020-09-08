167f1227c024bd7fc1938335cc93bf447caad9e5
kntmr <1657438+kntmr@users.noreply.github.com>
2020-09-08 23:02:06 +0900

    example 2-1 (Component)

---
+++ nuxt-examples/nuxt-testing-example/test/components/AppToggleButton.spec.js
@@ -0,0 +1,18 @@
+import AppToggleButton from '~/app/components/AppToggleButton.vue'
+import { mount } from '@vue/test-utils'
+
+describe('AppToggleButton.vue', () => {
+  let wrapper
+  beforeEach(() => {
+    wrapper = null
+    wrapper = mount(AppToggleButton)
+  })
+  test('default', () => {
+    expect(wrapper.find('p').text()).toBe('off')
+  })
+  test('on', async () => {
+    wrapper.find('button').trigger('click')
+    await wrapper.vm.$nextTick()
+    expect(wrapper.find('p').text()).toBe('on')
+  })
+})
